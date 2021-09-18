fc2ed82389a6dc5e1665f0ddf77d60b6b9d2931f
Jean Yoo <jy87@rice.edu>
2021-09-18 09:51:24 -0500

    m

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(({ reason, version }) => {
+  chrome.storage.sync.set({ color });
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
     showReadme();
   }
